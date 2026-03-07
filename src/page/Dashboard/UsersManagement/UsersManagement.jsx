65e73be7d991a83d06aa7a686368c26d4d792f1f
Muyedur rahman <muyedrahman732@gmail.com>
2026-03-07 19:12:13 +0600

    Toggle user Role Make Admin and Remove from admin

--- src/page/Dashboard/UsersManagement/UsersManagement.jsx
+++ src/page/Dashboard/UsersManagement/UsersManagement.jsx
@@ -1,81 +1,119 @@
-import React from 'react';
-import useAxiosSecure from '../../../hooks/useAxiosSecure';
-import { useQuery } from '@tanstack/react-query';
-import { FaUserShield } from 'react-icons/fa';
-import { FiShieldOff } from 'react-icons/fi';
-
-
+import React from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import { FaUserShield } from "react-icons/fa";
+import { FiShieldOff } from "react-icons/fi";
+import Swal from "sweetalert2";
 
 const UsersManagement = () => {
-    const axiosSecure = useAxiosSecure();
+  const axiosSecure = useAxiosSecure();
 
-    const {data: users = []} = useQuery({
-        queryKey: ['users'],
-        queryFn: async () =>{
-            const res = await axiosSecure.get(`/users`);
-            return res.data;
-        }
+  const {refetch, data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users`);
+      return res.data;
+    },
+  });
+
+  const handleMakeUser = user => {
+    const roleInfo = { role: "admin" };
+    axiosSecure.patch(`/users/${user._id}`, roleInfo)
+    .then((res) => {
+        console.log(res.data)
+      if (res.data.modifiedCount) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.displayName} marked as an Admin .`,
+          showConfirmButton: false,
+          timer: 2000,
+        });
+      }
+    });
+  };
+
+  const handleRemoveAdmin = user =>{
+    const roleInfo = {role: 'user'}
+    axiosSecure.patch(`/users/${user._id}`, roleInfo)
+    .then(res => {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.displayName} removed form Admin`,
+          showConfirmButton: false,
+          timer: 2000,
+        });
     })
+  }
 
-    return (
-      <div>
-        <h1 className="text-4xl">Management Users: {users.length}</h1>
+  return (
+    <div>
+      <h1 className="text-4xl">Management Users: {users.length}</h1>
 
-        <div className="overflow-x-auto">
-          <table className="table">
-            {/* head */}
-            <thead>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>User</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Admin Action</th>
+              <th>Others Actions</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
               <tr>
-                <th>#</th>
-                <th>User</th>
-                <th>Email</th>
-                <th>Role</th>
-                <th>Admin Action</th>
-                <th>Others Actions</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user, index) => (
-                <tr>
-                  <td>{index + 1}</td>
-                  <td>
-                    <div className="flex items-center gap-3">
-                      <div className="avatar">
-                        <div className="mask mask-squircle h-12 w-12">
-                          <img
-                            src={user.photoURL}
-                            alt="Avatar Tailwind CSS Component"
-                          />
-                        </div>
-                      </div>
-                      <div>
-                        <div className="font-bold"> {user.displayName}</div>
-                        <div className="text-sm opacity-50">United States</div>
+                <td>{index + 1}</td>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={user.photoURL}
+                          alt="Avatar Tailwind CSS Component"
+                        />
                       </div>
                     </div>
-                  </td>
-                  <td>{user.email}</td>
-                  <td>{user.role}</td>
-                  <td>
-                    {user.role === "admin" ? (
-                      <button className="btn">
-                        <FiShieldOff></FiShieldOff>
-                      </button>
-                    ) : (
-                      <button className="btn">
-                        <FaUserShield></FaUserShield>
-                      </button>
-                    )}
-                  </td>
-                  <th>Actions</th>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+                    <div>
+                      <div className="font-bold"> {user.displayName}</div>
+                      <div className="text-sm opacity-50">United States</div>
+                    </div>
+                  </div>
+                </td>
+                <td>{user.email}</td>
+                <td>{user.role}</td>
+                <td>
+                  {user.role === "admin" ? (
+                    <button
+                      onClick={() => handleRemoveAdmin(user)}
+                      className="btn bg-red-300"
+                    >
+                      <FiShieldOff></FiShieldOff>
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => handleMakeUser(user)}
+                      className="btn bg-green-400"
+                    >
+                      <FaUserShield></FaUserShield>
+                    </button>
+                  )}
+                </td>
+                <th>Actions</th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    );
+    </div>
+  );
 };
 
 export default UsersManagement;
