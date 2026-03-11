39647102dd6e51e6ab9267dbf9ec23ce13136d8b
Muyedur rahman <muyedrahman732@gmail.com>
2026-03-11 21:36:29 +0600

    Accept parcel and explore

--- src/page/Dashboard/AssignedDeliveries/AssignedDeliveries.jsx
+++ src/page/Dashboard/AssignedDeliveries/AssignedDeliveries.jsx
@@ -1,61 +1,88 @@
-import React from 'react';
-import useAuth from '../../../hooks/useAuth';
-import { useQuery } from '@tanstack/react-query';
-import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import React from "react";
+import useAuth from "../../../hooks/useAuth";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const AssignedDeliveries = () => {
-  const  {user} = useAuth();
+  const { user } = useAuth();
   const axiosSecure = useAxiosSecure();
 
+  const { data: parcels = [], refetch } = useQuery({
+    queryKey: ["parcels", user?.email, "driver_assigned"],
 
-  const {data: parcels = []} = useQuery({
-    queryKey: ['parcels', user?.email, 'driver_assigned'],
-
-    queryFn: async() =>{
+    queryFn: async () => {
       const res = await axiosSecure.get(
         `/parcels/rider?riderEmail=${user.email}&deliveryStatus=driver_assigned`,
       );
       return res.data;
-    }
-  })
+    },
+  });
 
+  const handleAcceptDelivery = (parcel) => {
+    const statusInfo = { deliveryStatus: "rider_arriving" };
+    axiosSecure
+      .patch(`/parcels/${parcel._id}/status`, statusInfo)
+      .then((res) => {
+        if (res.data.modifiedCount) {
+          refetch();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: `Thank you for accepting .`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+  };
 
-    return (
-      <div>
-        <h1 className="text-4xl">Parcels Pending Pickup: {parcels.length} </h1>
+  return (
+    <div>
+      <h1 className="text-4xl">Parcels Pending Pickup: {parcels.length} </h1>
 
-        <div className="overflow-x-auto">
-          <table className="table table-zebra">
-            {/* head */}
-            <thead>
-              <tr>
-                <th></th>
-                <th>Name</th>
-                <th> Conform</th>
-                <th>Favorite Color</th>
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th> Conform</th>
+              <th>Other Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {parcels.map((parcel, i) => (
+              <tr key={parcels._id}>
+                <th>{i + 1}</th>
+                <td>{parcel.parcelName}</td>
+                <td>
+                  {
+                  parcel.deliveryStatus === "driver_assigned"
+                 ? <>
+                    <button
+                    onClick={() => handleAcceptDelivery(parcel)}
+                    className="btn btn-primary text-black"
+                  >
+                    Accept
+                  </button>
+                  <button className="btn btn-warning ms-2 text-black">
+                    Riject
+                  </button>
+                  </>
+                  :
+                  <span>Accepted</span>
+                  }
+                
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {parcels.map((parcel, i) => (
-                <tr key={parcels._id}>
-                  <th>{i + 1}</th>
-                  <td>{parcel.parcelName}</td>
-                  <td>
-                    <button className='btn btn-primary text-black'>
-                      Accept
-                    </button>
-                    <button className='btn btn-warning ms-2 text-black'>
-                     Riject
-                    </button>
-                  </td>
-                  
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
       </div>
-    );
+    </div>
+  );
 };
 
 export default AssignedDeliveries;
