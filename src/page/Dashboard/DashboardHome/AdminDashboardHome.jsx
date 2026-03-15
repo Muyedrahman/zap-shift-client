d0feb68882703aa5a472895ea02e4cac9ee5a521
Muyedur rahman <muyedrahman732@gmail.com>
2026-03-15 21:27:24 +0600

    Implement Rechart & Show Delivery status in a chart

--- src/page/Dashboard/DashboardHome/AdminDashboardHome.jsx
+++ src/page/Dashboard/DashboardHome/AdminDashboardHome.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import { Legend, Pie, PieChart, Tooltip } from "recharts";
+
 
 const AdminDashboardHome = () => {
 
@@ -11,8 +12,13 @@ const AdminDashboardHome = () => {
       const res = await axiosSecure.get('/parcels/delivery-status/stats');
       return res.data;
     }
-  })
+  });
 
+  const getPieChartData = data =>{
+    return data.map(item => {
+      return {name: item.status, value: item.count}
+    })
+  }
 
   return (
     <div>
@@ -41,6 +47,32 @@ const AdminDashboardHome = () => {
             <div className="stat-desc">Jan 1st - Feb 1st</div>
           </div>
         ))}
+      </div>
+      <div className="w-full h-[400px]">
+        <PieChart
+          style={{
+            width: "100%",
+            maxWidth: "500px",
+            maxHeight: "80vh",
+            aspectRatio: 2,
+          }}
+          responsive
+        >
+          <Pie
+            dataKey="value"
+            startAngle={180}
+            endAngle={0}
+            data={getPieChartData(deliveryStats)}
+            cx="50%"
+            cy="100%"
+            outerRadius="120%"
+            fill="#8884d8"
+            label
+            isAnimationActive={true}
+          />
+          <Legend></Legend>
+          <Tooltip></Tooltip>
+        </PieChart>
       </div>
     </div>
   );
